412b349ba99c2236fec73651f24e815c38affb15
jmanager25 <jmanager25@hotmail.com>
2023-07-02 11:10:24 +0000

    Add navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import css from './App.module.css';
 import FunctionalSidebar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
+      {<NavBarSimple />}
       {<FunctionalSidebar />}
     </div>
   );

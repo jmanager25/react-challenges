b2208a33cdf0e8f6914b3ed10b42b22f0cfe35a8
jmanager25 <jmanager25@hotmail.com>
2023-07-14 09:30:12 +0000

    Component lifecycle

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import css from './App.module.css';
 import FunctionalSidebar from './components/Sidebar';
-import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* {<NavBarSimple />} */}
       {<NavBarForm />}
       {<FunctionalSidebar />}
       {<Content />}

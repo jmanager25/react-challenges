74184dcb7ff3e7dae239144712dd050c63995efb
jmanager25 <jmanager25@hotmail.com>
2023-07-18 08:31:37 +0000

    Network request

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,22 @@
-import css from './App.module.css';
-import FunctionalSidebar from './components/Sidebar';
-import NavBarForm from './components/NavBarForm';
-import ContentHooks from './components/ContentHooks';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
     <div className={css.App}>
-      {<NavBarForm />}
-      {<FunctionalSidebar />}
-      {<ContentHooks />}
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
     </div>
   );
 }
